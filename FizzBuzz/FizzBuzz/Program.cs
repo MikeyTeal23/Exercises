b3d1a1cfdb7482126fd8acb4f994acd1724db826
Mikey Teal <Mikey.Teal@softwire.com>
2017-09-05 15:20:34 +0100

    Works with multiples of 7

--- FizzBuzz/FizzBuzz/Program.cs
+++ FizzBuzz/FizzBuzz/Program.cs
@@ -14,17 +14,20 @@ namespace FizzBuzz
             bool divisBy3;
             bool divisBy5;
             bool divisBy7;
+            bool divisBy11;
             string result;
 
             for(int i = 1; i < 101; i++)
             {
                 divisBy3 = i % 3 == 0;
                 divisBy5 = i % 5 == 0;
+                divisBy7 = i % 7 == 0;
                 result = "";
 
                 //if (divisBy3 && divisBy5) { Console.WriteLine("FizzBuzz"); }
                 if (divisBy3) { result += "Fizz"; }
                 if (divisBy5) { result += "Buzz"; }
+                if (divisBy7) { result += "Bang";  }
                 if (result == "") { result = i.ToString(); }
 
                 Console.WriteLine(result);

2ee463640066539da286713ef15dd3c4817a54b9
Mikey Teal <Mikey.Teal@softwire.com>
2017-09-05 15:11:36 +0100

    Added bool variables

--- FizzBuzz/FizzBuzz/Program.cs
+++ FizzBuzz/FizzBuzz/Program.cs
@@ -11,12 +11,18 @@ namespace FizzBuzz
         static void Main(string[] args)
         {
 
+            bool divisBy3;
+            bool divisBy5;
+            bool divisBy7;
+
             for(int i = 1; i < 101; i++)
             {
+                divisBy3 = i % 3 == 0;
+                divisBy5 = i % 5 == 0;
 
-                if ((i % 3 == 0) && (i % 5 == 0)) { Console.WriteLine("FizzBuzz"); }
-                else if(i % 3 == 0) { Console.WriteLine("Fizz"); }
-                else if (i % 5 == 0) { Console.WriteLine("Buzz"); }
+                if (divisBy3 && divisBy5) { Console.WriteLine("FizzBuzz"); }
+                else if(divisBy3) { Console.WriteLine("Fizz"); }
+                else if (divisBy5) { Console.WriteLine("Buzz"); }
                 else { Console.WriteLine(i); }
             }
 

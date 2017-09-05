cdf6c3fff2df48235e92ae69fd199f5f991c5dfc
Mikey Teal <Mikey.Teal@softwire.com>
2017-09-05 15:17:47 +0100

    Altered to use output string

--- FizzBuzz/FizzBuzz/Program.cs
+++ FizzBuzz/FizzBuzz/Program.cs
@@ -14,16 +14,20 @@ namespace FizzBuzz
             bool divisBy3;
             bool divisBy5;
             bool divisBy7;
+            string result;
 
             for(int i = 1; i < 101; i++)
             {
                 divisBy3 = i % 3 == 0;
                 divisBy5 = i % 5 == 0;
+                result = "";
 
-                if (divisBy3 && divisBy5) { Console.WriteLine("FizzBuzz"); }
-                else if(divisBy3) { Console.WriteLine("Fizz"); }
-                else if (divisBy5) { Console.WriteLine("Buzz"); }
-                else { Console.WriteLine(i); }
+                //if (divisBy3 && divisBy5) { Console.WriteLine("FizzBuzz"); }
+                if (divisBy3) { result += "Fizz"; }
+                if (divisBy5) { result += "Buzz"; }
+                if (result == "") { result = i.ToString(); }
+
+                Console.WriteLine(result);
             }
 
         }

c4e407f67709f40a695aae66c78eb715d380d283
Mikey Teal <Mikey.Teal@softwire.com>
2017-09-06 11:05:05 +0100

    Now uses regex expressions

--- TLS/TLS/Program.cs
+++ TLS/TLS/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 
 namespace TLS
 {
@@ -15,17 +16,31 @@ namespace TLS
 
             string readText = System.IO.File.ReadAllText(filepath);
 
-            int counter = 0;
+            string regexPattern = "tra";
 
-            for(int i = 0 ; i < readText.Length - 2; i++)
-            {
-                if((readText[i] == 't') && (readText[i+1] == 'r') && (readText[i+2] == 'a'))
-                {
-                    counter++;
-                }
+            Regex rgx = new Regex(regexPattern, RegexOptions.IgnoreCase);
+
+            Match match = rgx.Match(readText);
+            int matchCount = 0;
+            while (match.Success) {
+                matchCount++;
+                match = match.NextMatch();
             }
 
-            Console.WriteLine(counter.ToString());
+            Console.WriteLine(matchCount.ToString());
+
+            //int counter = 0;
+
+            //for(int i = 0 ; i < readText.Length - 2; i++)
+            //{
+                
+            //    if((readText[i] == 't') && (readText[i+1] == 'r') && (readText[i+2] == 'a'))
+            //    {
+            //        counter++;
+            //    }
+            //}
+
+            //Console.WriteLine(counter.ToString());
 
         }
     }
